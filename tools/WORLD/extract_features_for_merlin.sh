f2a847c05b7da23bc4bb054e9bb4e9a8256de982
Srikanth Ronanki <srikanth.143.iiit@gmail.com>
2016-08-17 15:53:43 +0100

    added copy synthesis scripts for WORLD

--- tools/WORLD/extract_features_for_merlin.sh
+++ tools/WORLD/extract_features_for_merlin.sh
@@ -1,16 +1,17 @@
+#!/bin/sh
 
 # top merlin directory
-merlin_dir="/afs/inf.ed.ac.uk/group/cstr/projects/phd/s1432486/work/Merlin/Github_version/merlin"
+merlin_dir="/afs/inf.ed.ac.uk/group/cstr/projects/phd/s1432486/work/test/merlin"
 
 # tools directory
 world="${merlin_dir}/tools/WORLD/build"
 sptk="${merlin_dir}/tools/SPTK-3.7/bin"
 
 # input audio directory
-wav_dir="${merlin_dir}/tools/WORLD/wav_test"
+wav_dir="${merlin_dir}/dnn_baseline_practice/cmu_us_slt_arctic/wav"
 
 # Output features directory
-out_dir="${merlin_dir}/tools/WORLD/wav_test"
+out_dir="${merlin_dir}/dnn_baseline_practice/cmu_us_slt_arctic/data"
 
 sp_dir="${out_dir}/sp"
 mgc_dir="${out_dir}/mgc"
@@ -18,7 +19,6 @@ ap_dir="${out_dir}/ap"
 bap_dir="${out_dir}/bap"
 f0_dir="${out_dir}/f0"
 lf0_dir="${out_dir}/lf0"
-resyn_dir="${out_dir}/resyn_dir"
 
 mkdir -p ${out_dir}
 mkdir -p ${sp_dir}
@@ -26,7 +26,6 @@ mkdir -p ${mgc_dir}
 mkdir -p ${bap_dir}
 mkdir -p ${f0_dir}
 mkdir -p ${lf0_dir}
-mkdir -p ${resyn_dir}
 
 # initializations
 fs=16000
@@ -68,20 +67,8 @@ do
     ### convert bapd to bap ###
     $sptk/x2x +df ${bap_dir}/$file_id.bapd > ${bap_dir}/$file_id.bap
 
-    ### WORLD Re-synthesis -- reconstruction of parameters ###
-
-    ### convert lf0 to f0 ###
-    $sptk/sopr -magic -1.0E+10 -EXP -MAGIC 0.0 ${lf0_dir}/$file_id.lf0 | $sptk/x2x +fa > ${resyn_dir}/$file_id.resyn.f0a
-    $sptk/x2x +ad ${resyn_dir}/$file_id.resyn.f0a > ${resyn_dir}/$file_id.resyn.f0
-
-    ### convert mgc to sp ###
-    $sptk/mgc2sp -a $alpha -g 0 -m $mcsize -l $nFFTHalf -o 2 ${mgc_dir}/$file_id.mgc | $sptk/sopr -d 32768.0 -P | $sptk/x2x +fd > ${resyn_dir}/$file_id.resyn.sp
-   
-    ### convert bapd to bap ###
-    $sptk/x2x +fd ${bap_dir}/$file_id.bap > ${resyn_dir}/$file_id.resyn.bapd
-
-    $world/synth $nFFTHalf $fs ${resyn_dir}/$file_id.resyn.f0 ${resyn_dir}/$file_id.resyn.sp ${resyn_dir}/$file_id.resyn.bapd ${out_dir}/$file_id.resyn.wav
 done
 
-rm -rf $sp_dir $mgc_dir $f0_dir $lf0_dir $bap_dir
-rm -rf $resyn_dir
+rm -rf $sp_dir 
+rm -rf $f0_dir
+rm -rf $bap_dir/*.bapd
