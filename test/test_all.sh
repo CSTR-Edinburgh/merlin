3638e841bddd9a9d2fcd1a02996c629dbaaeca3e
Gilles Degottex <gad27@cam.ac.uk>
2016-08-24 15:12:54 +0100

    add a characterisation test using egs/slt_arctic/s1

---
+++ test/test_all.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Script for testing Merlin pipeline using characterization tests
+# This script has to be run in its directory, as shows the usage.
+
+
+# Some functions to extract results from logs
+
+get_duration_rmse_corr(){
+    cat $1 |grep "main: Test" |sed 's/^.*RMSE: \(.*\) frames.*CORR: \(.*\);/\1 \2/'
+}
+get_acoustic_mcd_bap_f0rmse_f0corr_vuv(){
+    cat $1 |grep "main: Test" |sed 's/^.*MCD: \(.*\) dB; BAP: \(.*\) dB;.*RMSE: \(.*\) Hz; CORR: \(.*\); VUV: \(.*\)%/\1 \2 \3 \4 \5/'
+}
+
+#  main ------------------------------------------------------------------------
+
+if test "$#" -ne 0; then
+    echo "Usage: ./test_all.sh"
+    exit 1
+fi
+
+
+TESTROOT=`pwd`
+
+FAILED=""
+
+GIT_DESCR=`git describe --tags --always`
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+GIT_DIFFS=`git diff`
+if [[ -z "$GITDIFFS" ]]; then
+    echo "The git indicates no difference against $GIT_BRANCH-$GIT_DESCR"
+    echo "(you're testing the git version, tests are supposed to pass flawlessly)"
+else
+    echo "There are differences against $GIT_BRANCH-$GIT_DESCR"
+    echo "(you're testing local changes, differences can be expected)"
+fi
+echo " "
+
+
+TESTNAME=charac_slt_short
+    CHARAC_SHORTEXP=../egs/slt_arctic/s1
+    echo "Characterization test on short data ($CHARAC_SHORTEXP)..."
+    cd $CHARAC_SHORTEXP
+
+    # Run the pipeline
+    ./run.sh
+
+    REF=$(get_duration_rmse_corr testrefs/slt_arctic_dnn_world/duration_model/log/DNN_TANH_TANH_TANH_TANH_LINEAR__dur_50_259_4_512_0.002000*.log)
+    TEST=$(get_duration_rmse_corr experiments/slt_arctic_dnn_world/duration_model/log/DNN_TANH_TANH_TANH_TANH_LINEAR__dur_50_259_4_512_0.002000*.log)
+    python $TESTROOT/test_compare.py --ref $REF --test $TEST --colnames RMSE CORR
+    if [[ "$?" == "1" ]]; then FAILED="$FAILED ${TESTNAME}_duration"; fi
+
+    REF=$(get_acoustic_mcd_bap_f0rmse_f0corr_vuv testrefs/slt_arctic_dnn_world/acoustic_model/log/DNN_TANH_TANH_TANH_TANH_LINEAR__mgc_lf0_vuv_bap_50_259_4_512_0.002000*.log)
+    TEST=$(get_acoustic_mcd_bap_f0rmse_f0corr_vuv experiments/slt_arctic_dnn_world/acoustic_model/log/DNN_TANH_TANH_TANH_TANH_LINEAR__mgc_lf0_vuv_bap_50_259_4_512_0.002000*.log)
+    python $TESTROOT/test_compare.py --ref $REF --test $TEST --colnames MCD BAP F0-RMSE F0-CORR VUV
+    if [[ "$?" == "1" ]]; then FAILED="$FAILED ${TESTNAME}_acoustic"; fi
+
+    echo " "
+
+# Add new tests here
+
+# TESTNAME=charac_slt_full ?
+
+
+    
+if [[ -n "$FAILED" ]]; then
+    echo "Failed tests: "$FAILED
+else
+    echo "All test passed!"
+fi
+
+cd $TESTROOT
---
+++ test/test_compare.py
@@ -0,0 +1,45 @@
+import sys
+import argparse
+
+import numpy as np
+
+def similar_reals(ref, test, tol, colnames=None):
+    '''
+    Compare vector test against vector ref with a tolerance of tol (common scalar or vector)
+    '''
+
+    ref = np.array(ref)
+    test = np.array(test)
+    tol = np.atleast_1d(tol)
+
+    if len(ref)!=len(test):
+        raise ValueError('Cannot compare arrays of different size')
+
+    if len(tol)==1:
+        tol = tol*np.ones(len(ref))
+
+
+    row_format ="{:>10}" * len(ref)
+
+    if any(abs(ref-test)>tol):
+        if colnames:
+            print('           '+row_format.format(*colnames))
+        print('Reference: '+row_format.format(*ref))
+        print('Test:      '+row_format.format(*test))
+        print('Diff:      '+row_format.format(*(ref-test)))
+        print('Tolerance: '+row_format.format(*tol))
+        print('FAILED')
+        return False
+
+    return True
+
+if __name__ == '__main__':
+    argpar = argparse.ArgumentParser()
+    argpar.add_argument("--ref", nargs='+', type=float, default=None, help="Reference values.")
+    argpar.add_argument("--test", nargs='+', type=float, help="Values to test against the references.")
+    argpar.add_argument("--tol", nargs='+', type=float, default=0.1, help="Accepted tolerance (if a single value is provided, it is used for all compared pairs")
+    argpar.add_argument("--colnames", nargs='+', default=None, help="Names for each column")
+    args = argpar.parse_args()
+
+    if not similar_reals(args.ref, args.test, args.tol, args.colnames):
+        sys.exit(1)
