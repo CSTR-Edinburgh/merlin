292a598210c4b93d0a75d3ecc7ad8bd87a44f666
Srikanth Ronanki <srikanth.143.iiit@gmail.com>
2016-08-30 20:33:11 +0100

    updated demo scripts

--- egs/slt_arctic/s1/run_demo.sh
+++ egs/slt_arctic/s1/run_demo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if test "$#" -ne 0; then
-    echo "Usage: ./run.sh"
+    echo "Usage: ./run_demo.sh"
     exit 1
 fi
 
 ### Step 1: setup directories and the training data files ###
 echo "Step 1: setting up experiments directory and the training data files..."
 global_config_file=conf/global_settings.cfg
-./scripts/setup.sh slt_arctic_dnn_world
+./scripts/setup.sh slt_arctic_demo
 ./scripts/prepare_config_files.sh $global_config_file
 
 if [ ! -f  $global_config_file ]; then
@@ -20,8 +20,8 @@ fi
 
 ### Step 2: train duration model ###
 echo "Step 2: training duration model..."
-./scripts/submit.sh ${MerlinDir}/src/run_merlin.py conf/duration_configfile.conf
+./scripts/submit.sh ${MerlinDir}/src/run_merlin.py conf/duration_${Voice}.conf
 
 ### Step 3: train acoustic model and synthesize speech ###
 echo "Step 3: training acoustic model and synthesizing speech..."
-./scripts/submit.sh ${MerlinDir}/src/run_merlin.py conf/acoustic_configfile.conf
+./scripts/submit.sh ${MerlinDir}/src/run_merlin.py conf/acoustic_${Voice}.conf
