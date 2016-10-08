3cc1aca867b50e70d1dd8e66dd59b79634a6c7c8
ronanki <srikanth.143.iiit@gmail.com>
2016-10-08 18:37:57 +0100

    update: scripts to perfrom alignment using festvox tools

--- misc/scripts/alignment/phone_align/README.md
+++ misc/scripts/alignment/phone_align/README.md
@@ -1,50 +1,23 @@
 Create training labels for merlin with festvox tools
 ----------------------------------------------------
 
-Step 0: Install [festvox] (http://festvox.org/download.html) and configure it's path.
+Step 0: Install [speech_tools] (http://festvox.org/packed/festival/2.4/speech_tools-2.4-release.tar.gz), [festival] (http://festvox.org/packed/festival/2.4/festival-2.4-release.tar.gz), 
+and [festvox] (http://festvox.org/download.html).
 
-Below here, we provide some steps which are adapted from: <br/>
+Step 1: Run setup.sh -- to download slt data and to create the config file. <br/>
+./setup.sh
+
+Step 2: Please configure paths to all tools in config.cfg
+
+Step 3: Run aligner.sh -- which uses ehmm in clustergen setup (from festvox tools) to do forced-alignment. <br/>
+./run_aligner.sh config.cfg
+
+If all the above steps performed successfully, you should have your labels ready !! :)
+
+EHMM Aligner
+------------
+
+The steps to perform ehmm alignment are adapted from: <br/>
 [http://festvox.org/bsv/c3174.html] (http://festvox.org/bsv/c3174.html)
 
-Step 1: mkdir cmu_us_slt_arctic <br/>
-Step 2: cd cmu_us_slt_arctic <br/>
-
-Step 3: $FESTVOXDIR/src/clustergen/setup_cg cmu us slt_arctic <br/>
-
-Step 4: Download [cmuarctic.data] (http://festvox.org/cmu_arctic/cmuarctic.data) and copy into etc/txt.done.data <br/>
-a) wget http://festvox.org/cmu_arctic/cmuarctic.data <br/>
-b) cp cmuarctic.data etc/txt.done.data
-
-Step 4: ./bin/do_build build_prompts <br/>
-Step 5: ./bin/do_build label <br/>
-
-After all above steps have been performed successfully, you should be able to find the labelled data <br/>
-in the directory "lab" and corresponding utts in "festival/utts".
-
-From here, you should use the scripts in Merlin to convert festival utts to full-contextual phone labels.
-
-Step 6: cd ../
-
-Step 7: configure frontend and FESTDIR paths. <br/>
-a) frontend=${MerlinDir}/misc/scripts/frontend <br/>
-b) FESTDIR=${MerlinDir}/tools/festival
-
-Step 8: create a file id list <br/>
-cat slt_arctic_demo_data/cmuarctic.data | cut -d " " -f 2 > file_id_list.scp
-
-Step 9: convert festival utts to lab <br/>
-${frontend}/festival_utt_to_lab/make_labels \ <br/>
-                            full-context-labels \ <br/>
-                            cmu_us_slt_arctic/festival/utts \ <br/>
-                            ${FESTDIR}/examples/dumpfeats \ <br/>
-                            ${frontend}/festival_utt_to_lab 
-
-Step 10: normalize lab for merlin <br/>
-python ${frontend}/utils/normalize_lab_for_merlin.py \ <br/>
-                            full-context-labels/full \ <br/>
-                            label_phone_align \ <br/>
-                            phone_align \ <br/>
-                            file_id_list.scp <br/>
-
-You should have your labels ready !! :)
 
--- misc/scripts/alignment/phone_align/run_aligner.sh
+++ misc/scripts/alignment/phone_align/run_aligner.sh
@@ -1,45 +1,29 @@
 #!/bin/bash -e
 
-if test "$#" -ne 0; then
-    echo "Usage: ./run_aligner.sh"
+if test "$#" -ne 1; then
+    echo "Usage: ./run_aligner.sh config.cfg"
     exit 1
+fi
+
+if [ ! -f $1 ]; then
+    echo "Config file doesn't exist"
+    exit 1
+else
+    source $1
 fi
 
 #################################################################
 ##### Create training labels for merlin with festvox tools ######
 #################################################################
 
-### Download data -- slt cmuarctic.data
+### tools required
 
-audio_data=slt_wav
-
-audio_data_url=http://104.131.174.95/${audio_data}.zip
-text_data_url=http://festvox.org/cmu_arctic/cmuarctic.data
-
-if [[ ! -f ${audio_data}.zip ]]; then
-    echo "downloading data....."
-    rm -f ${audio_data}.zip
-    if hash curl 2>/dev/null; then
-        curl -O $audio_data_url
-        curl -O $text_data_url
-    elif hash wget 2>/dev/null; then
-        wget $audio_data_url
-        wget $text_data_url
-    else
-        echo "please download the audio data from $audio_data_url"
-        echo "please download the text data from $text_data_url"
-        exit 1
-    fi
-    do_unzip=true
+if [[ ! -d "${ESTDIR}" ]] || [[ ! -d "${FESTDIR}" ]] || [[ ! -d "${FESTVOXDIR}" ]]; then
+    echo "Please configure paths to speech_tools, festival and festvox in config.cfg !!"
+    exit 1
 fi
 
-if [[ ! -d ${audio_data} ]] || [[ -n "$do_unzip" ]]; then
-    unzip -q ${audio_data}.zip
-fi
-
-### do forced alignment using ehmm in clustergen
-
-FESTVOXDIR=${MerlinDir}/tools/festvox
+### do forced alignment using ehmm in clustergen setup
 
 mkdir cmu_us_slt_arctic
 cd cmu_us_slt_arctic
@@ -50,30 +34,28 @@ cp ../cmuarctic.data etc/txt.done.data
 cp ../slt_wav/*.wav wav/
 
 ./bin/do_build build_prompts 
-./bin/do_build label 
+./bin/do_build label
+./bin/do_build build_utts
 
 cd ../
 
 ### convert festival utts to lab
 
-frontend=${MerlinDir}/misc/scripts/frontend 
-FESTDIR=${MerlinDir}/tools/festival
-
-head cmuarctic.data | cut -d " " -f 2 > file_id_list.scp
+cat cmuarctic.data | cut -d " " -f 2 > file_id_list.scp
 
 echo "converting festival utts to labels..."
-${frontend}/festival_utt_to_lab/make_labels \ 
-                            full-context-labels \ 
-                            cmu_us_slt_arctic/festival/utts \ 
-                            ${FESTDIR}/examples/dumpfeats \ 
-                            ${frontend}/festival_utt_to_lab 
+${frontend}/festival_utt_to_lab/make_labels \
+                        full-context-labels \
+                        cmu_us_slt_arctic/festival/utts \
+                        ${FESTDIR}/examples/dumpfeats \
+                        ${frontend}/festival_utt_to_lab 
 
 echo "normalizing label files for merlin..."
-python ${frontend}/utils/normalize_lab_for_merlin.py \ 
-                            full-context-labels/full \ 
-                            label_phone_align \ 
-                            phone_align \ 
-                            file_id_list.scp 
+python ${frontend}/utils/normalize_lab_for_merlin.py \
+                        full-context-labels/full \
+                        label_phone_align \
+                        phone_align \
+                        file_id_list.scp 
 
 echo "You should have your labels ready in: label_phone_align !!"
 
