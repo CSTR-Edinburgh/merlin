df7e3494b0662fec3a2fbe4d51d056e80cc6b682
ronanki <srikanth.143.iiit@gmail.com>
2016-10-19 11:41:36 +0100

    update prepare data script in build own voice

--- egs/build_your_own_voice/s1/02_prepare_data.sh
+++ egs/build_your_own_voice/s1/02_prepare_data.sh
@@ -10,6 +10,18 @@ PrintUsage () {
     echo "To run:"
     echo "Step 1: ./01_setup.sh give_your_voice_name"
 }
+PrintUsageOne () {
+    echo ""
+    echo "Please place your audio files in: ${wav_dir}"
+    echo ""
+}
+PrintUsageTwo () {
+    echo ""
+    echo "Please give input: either 1 or 2"
+    echo "1. ${txt_dir}  -- a text directory containing text files"
+    echo "2. ${txt_file} -- a single text file with each sentence in a new line in festival format"
+    echo ""
+}
 
 global_config_file=conf/global_settings.cfg
 
@@ -22,21 +34,21 @@ else
 fi
 
 ### define few variables here
-data_dir=${current_working_dir}/database
+#data_dir=${WorkDir}/database
+data_dir=database
 
 wav_dir=${data_dir}/wav
 txt_dir=${data_dir}/txt
 txt_file=${data_dir}/utts.data
 
-if [[ ! -d "${data_dir}" ]]; then
-    PrintUsage
+if [ ! "$(ls -A ${wav_dir})" ]; then
+    PrintUsageOne
+    PrintUsageTwo
     exit 1
 fi
 
 if [[ ! -d "${txt_dir}" ]] && [[ ! -f "${txt_file}" ]]; then
-    echo "Please give input: either 1 or 2"
-    echo "1. ${txt_dir}  -- a text directory containing text files"
-    echo "2. ${txt_file} -- a single text file with each sentence in a new line in festival format"
+    PrintUsageTwo
     exit 1
 fi
 
@@ -45,13 +57,27 @@ fi
 ##### prepare vocoder features #####
 ####################################
 
-echo "Follow the scripts in:"
+echo ""
+echo "Step 1: Prepare vocoder features"
+echo "To prepare vocoder features, follow the scripts in:"
 echo "https://github.com/CSTR-Edinburgh/merlin/tree/master/misc/scripts/vocoder"
+echo ""
 
 ####################################
 ######## prepare labels ############
 ####################################
 
-echo "Follow the scripts in:"
+echo ""
+echo "Step 2: Prepare labels"
+echo "To prepare labels, follow the scripts in:"
 echo "https://github.com/CSTR-Edinburgh/merlin/tree/master/misc/scripts/alignment"
+echo ""
 
+################################################
+######## automatic data preparation ############
+################################################
+
+echo ""
+echo "Scripts for automatic data preparation are still under development."
+echo "If you want to contribute, this is the choice, please let us know!! :)"
+echo ""
