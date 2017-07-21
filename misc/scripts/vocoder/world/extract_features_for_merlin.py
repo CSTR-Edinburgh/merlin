c994918a7366598d76b7c31a8c88555d069a90b4
tuanad121 <tuanad121@gmail.com>
2017-07-21 04:45:07 -0700

    Multi-processing analysis STRAIGHT and WORLD (#195)

--- misc/scripts/vocoder/world/extract_features_for_merlin.py
+++ misc/scripts/vocoder/world/extract_features_for_merlin.py
@@ -2,6 +2,7 @@ import os
 import sys
 import shutil
 import glob
+import multiprocessing as mp
 
 if len(sys.argv)!=5:
     print("Usage: ")
@@ -71,48 +72,60 @@ def get_wav_filelist(wav_dir):
 
 get_wav_filelist(wav_dir)
 
-for file in wav_files:
-    if file is not None:
-        file_id = os.path.basename(file).split(".")[0]
-        print(('\n'+file_id))
-        ### WORLD ANALYSIS -- extract vocoder parameters ###
-        ### extract f0, sp, ap ###
-        world_analysis_cmd = "%s %s %s %s %s" %(os.path.join(world,'analysis'), \
-                                                file,
-                                             os.path.join(f0_dir,file_id+'.f0'), \
+
+def process(filename):
+    '''
+    The function decomposes a wav file into F0, mel-cepstral coefficients, and aperiodicity
+    :param filename: path to wav file
+    :return: .lf0, .mgc and .bap files
+    '''
+    file_id = os.path.basename(filename).split(".")[0]
+    print('\n' + file_id)
+    ### WORLD ANALYSIS -- extract vocoder parameters ###
+    ### extract f0, sp, ap ###
+    world_analysis_cmd = "%s %s %s %s %s" % (os.path.join(world, 'analysis'), \
+                                             filename,
+                                             os.path.join(f0_dir, file_id + '.f0'), \
                                              os.path.join(sp_dir, file_id + '.sp'), \
                                              os.path.join(bap_dir, file_id + '.bapd'))
-        os.system(world_analysis_cmd)
+    os.system(world_analysis_cmd)
 
-        ### convert f0 to lf0 ###
-        sptk_x2x_da_cmd = "%s +da %s > %s"%(os.path.join(sptk,'x2x'),\
-                                          os.path.join(f0_dir,file_id+'.f0'), \
+    ### convert f0 to lf0 ###
+    sptk_x2x_da_cmd = "%s +da %s > %s" % (os.path.join(sptk, 'x2x'), \
+                                          os.path.join(f0_dir, file_id + '.f0'), \
                                           os.path.join(f0_dir, file_id + '.f0a'))
-        os.system(sptk_x2x_da_cmd)
+    os.system(sptk_x2x_da_cmd)
 
-        sptk_x2x_af_cmd = "%s +af %s | %s > %s " % (os.path.join(sptk, 'x2x'), \
-                                              os.path.join(f0_dir, file_id + '.f0a'), \
-                                              os.path.join(sptk,'sopr')+' -magic 0.0 -LN -MAGIC -1.0E+10',\
-                                              os.path.join(lf0_dir,file_id+'.lf0'))
-        os.system(sptk_x2x_af_cmd)
+    sptk_x2x_af_cmd = "%s +af %s | %s > %s " % (os.path.join(sptk, 'x2x'), \
+                                                os.path.join(f0_dir, file_id + '.f0a'), \
+                                                os.path.join(sptk, 'sopr') + ' -magic 0.0 -LN -MAGIC -1.0E+10', \
+                                                os.path.join(lf0_dir, file_id + '.lf0'))
+    os.system(sptk_x2x_af_cmd)
 
-        ### convert sp to mgc ###
-        sptk_x2x_df_cmd1 = "%s +df %s | %s | %s >%s" % (os.path.join(sptk, 'x2x'), \
+    ### convert sp to mgc ###
+    sptk_x2x_df_cmd1 = "%s +df %s | %s | %s >%s" % (os.path.join(sptk, 'x2x'), \
                                                     os.path.join(sp_dir, file_id + '.sp'), \
                                                     os.path.join(sptk, 'sopr') + ' -R -m 32768.0', \
-                                                    os.path.join(sptk, 'mcep')+' -a '+str(alpha)+' -m '+ str(mcsize)+' -l '+str(nFFTHalf)+' -e 1.0E-8 -j 0 -f 0.0 -q 3 ',\
-                                                    os.path.join(mgc_dir,file_id+'.mgc'))
-        os.system(sptk_x2x_df_cmd1)
+                                                    os.path.join(sptk, 'mcep') + ' -a ' + str(alpha) + ' -m ' + str(
+                                                        mcsize) + ' -l ' + str(
+                                                        nFFTHalf) + ' -e 1.0E-8 -j 0 -f 0.0 -q 3 ', \
+                                                    os.path.join(mgc_dir, file_id + '.mgc'))
+    os.system(sptk_x2x_df_cmd1)
 
-        ### convert bapd to bap ###
-        sptk_x2x_df_cmd2 = "%s +df %s > %s "%(os.path.join(sptk,"x2x"),\
-                                            os.path.join(bap_dir,file_id+".bapd"),\
-                                            os.path.join(bap_dir,file_id+'.bap'))
-        os.system(sptk_x2x_df_cmd2)
+    ### convert bapd to bap ###
+    sptk_x2x_df_cmd2 = "%s +df %s > %s " % (os.path.join(sptk, "x2x"), \
+                                            os.path.join(bap_dir, file_id + ".bapd"), \
+                                            os.path.join(bap_dir, file_id + '.bap'))
+    os.system(sptk_x2x_df_cmd2)
 
+# do multi-processing
+with mp.Pool(mp.cpu_count()) as pool:
+    pool.map(process, wav_files)
+
+# clean temporal files
 shutil.rmtree(sp_dir, ignore_errors=True)
 shutil.rmtree(f0_dir, ignore_errors=True)
 for zippath in glob.iglob(os.path.join(bap_dir, '*.bapd')):
     os.remove(zippath)
 
-print(("You should have your features ready in: "+out_dir))
+print("You should have your features ready in: "+out_dir)    
