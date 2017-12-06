faf947ff7137fb8781eca753f7dcfc2b71bb917d
ronanki <srikanth.143.iiit@gmail.com>
2017-12-06 18:45:51 +0000

    update voice conversion demo with magphase vocoder

--- egs/voice_conversion/s2/run_demo_vc.sh
+++ egs/voice_conversion/s2/run_demo_vc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-setup_data=false
+setup_data=true
 train_vc=true
-run_vc=false
+run_vc=true
 
 if test "$#" -ne 2; then
     src_speaker="bdl"
@@ -38,27 +38,27 @@ fi
 # train voice conversion system for any source-target pair
 if [ "$train_vc" = true ]; then
     # step 1: run setup
-    #./01_setup.sh $src_speaker $tgt_speaker
+    ./01_setup.sh $src_speaker $tgt_speaker
 
     # step 2: extract acoustic features
-    #./02_prepare_acoustic_features.sh database/$src_speaker/wav/ database/$src_speaker/feats
-    #./02_prepare_acoustic_features.sh database/$tgt_speaker/wav/ database/$tgt_speaker/feats
+    ./02_prepare_acoustic_features.sh database/$src_speaker/wav/ database/$src_speaker/feats
+    ./02_prepare_acoustic_features.sh database/$tgt_speaker/wav/ database/$tgt_speaker/feats
 
     # step 3: align source features with target (create parallel data)
-    #./03_align_src_with_target.sh database/$src_speaker/feats database/$tgt_speaker/feats database/${src_speaker}_aligned_with_${tgt_speaker}/feats
+    ./03_align_src_with_target.sh database/$src_speaker/feats database/$tgt_speaker/feats database/${src_speaker}_aligned_with_${tgt_speaker}/feats
 
     # step 4: prepare config files for training and testing
-    #./04_prepare_conf_files.sh conf/global_settings.cfg
+    ./04_prepare_conf_files.sh conf/global_settings.cfg
 
     # step 5: train voice-conversion model
     # prepare acoustic features for source
     ./05_train_acoustic_model.sh conf/acoustic_${src_speaker}.conf
 
     # create a symbolic link for source features in target-voice directory
-    #./scripts/create_symbolic_link.sh
+    ./scripts/create_symbolic_link.sh
 
     # train an acoustic model with mapping from source to target
-    #./05_train_acoustic_model.sh conf/acoustic_${src_speaker}2${tgt_speaker}.conf
+    ./05_train_acoustic_model.sh conf/acoustic_${src_speaker}2${tgt_speaker}.conf
 fi
 
 # run voice conversion
